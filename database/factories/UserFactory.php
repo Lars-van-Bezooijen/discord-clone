9ac452d44fbbb2e802be738c4c1b0d04a5ab80fe
Lars-van-Bezooijen <latsvb10@gmail.com>
2023-06-08 21:00:29 +0200

    added seeders for users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->unique()->userName(),
+            // display name is either a random name or null
+            'display_name' => fake()->boolean(50) ? fake()->name() : null,
+            'status_id' => 1,
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
