2030d0ea4fba0e366be7e66445b743dad8daaab6
Lars-van-Bezooijen <latsvb10@gmail.com>
2023-06-09 14:01:49 +0200

    Removed old code login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,55 +1,21 @@
-{{-- 
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
- --}}
-
 @extends('layouts.base')
 @section('content')
 
 <div class="select-none w-full h-full sm:bg-[url({{ asset('images/discord_background.jpg') }})] bg-no-repeat bg-center bg-cover flex justify-center items-center">
+    {{-- Container --}}
     <div class="bg-discord-backgroundlight pt-6 px-6 sm:p-6 sm:m-6 h-full sm:h-auto sm:rounded-md w-full md:max-w-[500px]">
+        {{-- Heading --}}
         <div class="text-center mb-4">
             <img src="{{ asset('images/discord_logo_with_text.png') }}" alt="logo" class="w-32 mb-6 m-auto sm:hidden">
             <p class="text-xl font-bold">Welcome back!</p>
             <p class="text-discord-graytext">We're so excited to see you again!</p>
         </div>
         
+        {{-- Content --}}
         <form action="{{ route('login') }}" method="POST">
             @csrf
             
+            {{-- Email --}}
             <div class="flex flex-col mb-4">
                 @if($errors->any())
                     <label for="email" class="text-discord-error font-bold text-xs mb-2">EMAIL - <span class="italic font-semibold">Login or password is invalid</span></label>
@@ -58,6 +24,7 @@
                 @endif
                 <input type="email" name="email" id="email" class="bg-discord-backgrounddark border-none rounded-md p-2 focus:ring-0">
             </div>
+            {{-- Password --}}
             <div class="flex flex-col">
                 @if($errors->any())
                     <label for="email" class="text-discord-error font-bold text-xs mb-2">PASSWORD - <span class="italic font-semibold">Login or password is invalid</span></label>
@@ -66,15 +33,18 @@
                 @endif
                 <input type="password" name="password" id="password" class="bg-discord-backgrounddark border-none rounded-md p-2 focus:ring-0">
             </div>
+            {{-- Forgot password --}}
             <a href="{{ route('password.request') }}" class="basic-link inline-block mb-4">Forgot your password?</a>
+            {{-- Submit --}}
             <div>
                 <div class="bg-discord-blue hover:bg-discord-bluehover mb-2 rounded-md">
                     <button type="submit" class="w-full text-white p-2 font-semibold rounded-md focus:ring-discord-bluetext focus:ring-2 focus:outline-none">Log In</button>
                 </div>
+                {{-- Register link --}}
                 <p class="text-sm text-discord-graytext">Need an account? <a href="{{ route('register') }}" class="basic-link">Register</a></p>
             </div>
-        </form>
-    </div>
+        </form> {{-- End of form --}}
+    </div> {{-- End of container --}}
 </div>
 
 @endsection
